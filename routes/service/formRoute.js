80b83905d73bcfe42339b179d0ad6bf2441f4a3a
Prathamesh010 <prathameshgawas87@gmail.com>
2021-03-28 11:43:25 +0530

    fix form enctype

--- routes/service/formRoute.js
+++ routes/service/formRoute.js
@@ -1,43 +1,51 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-var multer  = require('multer')
-const Response = require('../../models/Response')
-const {FirebaseUpload} = require('../../utils/uploadFile')
+var multer = require("multer")
+const Response = require("../../models/Response")
+const { FirebaseUpload } = require("../../utils/uploadFile")
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
+  if (file.mimetype == "image/jpeg" || file.mimetype == "image/png") {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
 }
 
-const upload = multer({ storage: multer.memoryStorage(), fileFilter: fileFilter })
-
-router.post("/form",upload.single('upload'),async (req,res)=>{
-    const {name,radio} = req.body
-
-    FirebaseUpload(req.file,'paymentImages').then(async (url) =>{
-        const newResponse = new Response({
-            name: name,
-            radio: radio,
-            imageUrl: url
-        })
-        await newResponse.save()
-        res.redirect('/form')
-    }).catch((err)=>{
-        res.status(200).json({
-            "err": err
-        })
-        res.end();
-    });
+const upload = multer({
+  storage: multer.memoryStorage(),
+  fileFilter: fileFilter,
 })
 
-router.get("/form",(req,res)=>{
-    Response.find({},(err,data)=> {
-        if(err) res.send(err) 
-        else res.send(data)
+router.post("/form", upload.single("upload"), async (req, res) => {
+  const { name, radio } = req.body
+  console.log(req.body)
+  console.log(req.file)
+
+  FirebaseUpload(req.file, "paymentImages")
+    .then(async (url) => {
+      console.log(url)
+      const newResponse = new Response({
+        name: name,
+        radio: radio,
+        imageUrl: url,
+      })
+      await newResponse.save()
+      res.redirect("/")
     })
+    .catch((err) => {
+      res.status(200).json({
+        err: err,
+      })
+      res.end()
+    })
+})
+
+router.get("/form", (req, res) => {
+  Response.find({}, (err, data) => {
+    if (err) res.send(err)
+    else res.send(data)
+  })
 })
 
 module.exports = router
